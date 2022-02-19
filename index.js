dc09bedb5d1f482c9a8cdd37be2183fb929967e6
unknown <ryanadalal@gmail.com>
2022-02-18 22:21:21 -0500

    working

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const nodemailer = require('nodemailer');
 const app = express();
 dotenv.config();
 
+app.use(express.urlencoded({ extended: false }));
+
 var transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
@@ -14,22 +16,62 @@ var transporter = nodemailer.createTransport({
 });
 const mailOptions = {
   from: process.env.EMAIL,
-  to: process.env.TESTINGEMAIL,
-  subject: 'Testing Email',
-  html: '<h1>Testing</h1> <p>Test</p>'
+  subject: 'Ticket Confirmation'
 };
 
-/*transporter.sendMail(mailOptions, function(error, info){
-  if (error) {
-	console.log(error);
-  } else {
-    console.log('Email sent: ' + info.response);
-  }
-});*/
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/client/index.html');
 });
+
+app.get('/sucess', (req, res) => {
+  res.sendFile(__dirname + '/client/sucess.html');
+});
+
+app.post('/submit', (req, res) => {
+  sendTicket(req.body.firstname, req.body.lastname, req.body.email, req.body.subject, res);
+});
+
+function sendConfirmation(first, last, email, subject, res){
+  mailOptions.to = email;
+  mailOptions.html = `
+  <body style="background-color: lightgrey; font-family:cursive">
+    <h2>Hello ${first} ${last},</h2>
+    <h3>This is a confirmation for your ticket.</h3>
+    
+    <p style="font-weight: bold;">Your message: </p>
+    <p>${subject}</p>
+  </body>
+  `;
+  transporter.sendMail(mailOptions, function(err, info){
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('Confirm sent: ' + info.response);
+      res.redirect('/sucess');
+    }
+  });
+}
+
+function sendTicket(first, last, email, subject, res){
+  mailOptions.to = process.env.EMAIL;
+  mailOptions.html = `
+  <body style="background-color: lightgrey; font-family:cursive">
+    <h2>Ticket from ${first} ${last},</h2>
+    <h3>Email: ${email}</h3>
+    <p style="font-weight: bold;">Message: </p>
+    <p>${subject}</p>
+  </body>
+  `;
+  transporter.sendMail(mailOptions, function(err, info){
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('Ticket sent: ' + info.response);
+      sendConfirmation(first, last, email, subject, res);
+    }
+  });
+}
 
 app.listen(process.env.PORT, (err) => {
   console.log('Listening on ' + process.env.PORT + '...');
